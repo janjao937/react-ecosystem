299c580edbba3c2e9476cae82b3d5540157fa7d5
janjao937 <janjao937@gmail.com>
2023-09-08 10:01:01 +0700

    change theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const App =()=>{
+  const [isDatkMode,ediMode] = useState(false);
+
+  const OnChangeModeHandler =()=>{
+    ediMode(!isDatkMode);
+  }
+  const styleObj = {
+    backgroundColor : isDatkMode?"black":"white",
+    color:isDatkMode?"white":"black",
+
+  }
+  const btnObj = {
+    backgroundColor : isDatkMode?"white":"black",
+    color:isDatkMode?"black":"white",
+
+  }
+  return(
+    <div className='App' style={styleObj}>
+      <h1>Banana</h1>
+      <button style={btnObj} onClick={OnChangeModeHandler}>Change Theme</button>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
