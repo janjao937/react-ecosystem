7a96f075666df2d382bd4676427d9e80ae4c0c53
janjao937 <janjao937@gmail.com>
2023-09-08 12:09:12 +0700

    use axios in OnAuthHandler

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect,useContext } from 'react'
 import ReactDOM from 'react-dom/client'
-
+import axios from "axios";
 import './index.css'
 
 const AuthCtx = React.createContext();
@@ -8,7 +8,7 @@ const AuthCtx = React.createContext();
 const AuthContexProvider =(p)=>{
   const [isAuth,setIsAuth] = useState(false);
   const [isLoading,setIsLoading] = useState(false);
-
+  const [user,setUser] = useState({user:"guest"});
   //delay loading
   useEffect(()=>{
     if(isLoading)
@@ -17,20 +17,28 @@ const AuthContexProvider =(p)=>{
     }
   },[isLoading]);
 
-  const OnAuthHandler =()=>{
-    let auth = isAuth;
-    if(!auth)
-    {
-      setIsLoading(true);
-      setIsAuth(true);
-    }
-    else{
+  const OnAuthHandler = async()=>{
+    if(isAuth){
       setIsAuth(false);
+      setUser({user:"guest"});
+      return;
     }
     
-    // setIsAuth(!isAuth);
+    try{
+      setIsLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+      setUser(res.data);
+      setIsAuth(true);
+
+    }catch(err){
+      console.log(err);
+    }finally{
+      setTimeout(()=>{
+        setIsLoading(false);
+      },1000);
+    }
   }
-  const share ={isAuth,OnAuthHandler,isLoading};
+  const share ={isAuth,OnAuthHandler,isLoading,user};
   return (
   <AuthCtx.Provider value={share}>
     {p.children}
@@ -40,11 +48,11 @@ const AuthContexProvider =(p)=>{
 
 const App=()=>{
 
-  const {isAuth,OnAuthHandler,isLoading} = useContext(AuthCtx);
+  const {isAuth,OnAuthHandler,isLoading,user} = useContext(AuthCtx);
   
   return(
     <div className='App'>
-      {isLoading?<h1>Loading...</h1>:<h1>Welcome:{!isAuth?"Guest":"User"}</h1>}
+      {isLoading?<h1>Loading...</h1>:<h1>Welcome:{!isAuth?"Guest":user.name}</h1>}
       <button onClick={OnAuthHandler}>{!isAuth?"Login":"Logout"}</button>
       
     </div>
