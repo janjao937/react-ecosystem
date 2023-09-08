dd2825eb4a2e5c20954f9d0bff6336fac737a9c1
janjao937 <janjao937@gmail.com>
2023-09-08 15:33:01 +0700

    basic use createBrowserRouter,RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter,createBrowserRouter,RouterProvider} from "react-router-dom";
 
-import './index.css'
+import './index.css';
 
-const App=()=>{
-  return(
-    <div>
-      Hello
-    </div>
-  )
-}
+// createBrowserRouter([])
+const routes = [
+    {path:"/",element:<div>Home Page</div>},
+    {path:"/profile",element:<div>Profile</div>},
+    {path:"*",element:<div>404:Not found</div>}
+]
+const router =createBrowserRouter(routes)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App/>
+  <RouterProvider router={router}/>
 )
